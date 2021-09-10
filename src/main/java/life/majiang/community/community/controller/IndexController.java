3ca1850ac48536b9617f836da7f1f6f4c21b0ce4
guolin <wangguolin754@163.com>
2021-09-10 22:09:03 +0800

    init rep

---
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -0,0 +1,14 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")//一个反斜杠都不写，表示根目录
+    public String index(){
+        return "index";
+    }
+
+}
