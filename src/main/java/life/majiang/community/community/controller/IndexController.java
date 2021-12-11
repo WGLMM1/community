d0043e94068dff28d42fef761bdb02ca7f3c4761
WGL <wgl@WGLdeMacBook-Pro.local>
2021-12-11 19:54:39 +0800

    test push

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")//一个反斜杠都不写，表示根目录
+    @GetMapping("/")//一个反斜杠都不写，表示根目录s
     public String index(){
         return "index";
     }
